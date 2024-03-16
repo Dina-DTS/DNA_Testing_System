353fc304df2e4b2a0849fc15be35f086ecb18dce
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2024-03-16 20:34:35 +0200

    aaa

--- src/modules/population/router/population.router.js
+++ src/modules/population/router/population.router.js
@@ -9,12 +9,6 @@ import getTextFileUploadMiddleware from "../../../middlewares/upload.js";
 const router=Router()
 
 router.post("/addpopulation",authenticate,autherize('technical'),getTextFileUploadMiddleware(),validate(AddPopulationSchema,AddPopulationlQuery),asyncHandler(addPopulation))
-
-// import upload from "../../../middlewares/fileUpload.middleware.js";
-
-
-// router.post('/add',authenticate,autherize('technical'),validate(AddPopulationSchema,AddPopulationlQuery), upload.single('file'),asyncHandler(addPopulation));
-
 router.get('/getAllPopulation',authenticate,autherize('admin'),asyncHandler(getAllPopulation))
 router.get('/identification',authenticate,autherize('admin','technical'),asyncHandler(identification))
 router.post('/identificationByDNA',authenticate,autherize('admin','technical'),getTextFileUploadMiddleware(),asyncHandler(identificationByDNA))
