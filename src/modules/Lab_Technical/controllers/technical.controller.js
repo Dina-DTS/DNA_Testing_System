c47079f6bd9eeb0d900e443cf0ad321373782294
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2024-04-20 20:42:51 +0200

    pp

--- src/modules/Lab_Technical/controllers/technical.controller.js
+++ src/modules/Lab_Technical/controllers/technical.controller.js
@@ -229,7 +229,7 @@ export const signInTechnical = asyncHandler(async (req, res) => {
     
   return res.status(200).json({
     message: 'Sign in successfully',
-    expiresIn: '12h',
+    expiresIn: '12',
     statusCode:200,
     newToken:token,
     technical:technicalPersonnel
