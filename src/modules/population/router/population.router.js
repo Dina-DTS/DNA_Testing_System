d7892d63cdc8baa3300994debd8fe691b1f76b53
mariamhesham12 <99356287+mariamhesham12@users.noreply.github.com>
2024-05-19 00:14:24 +0300

    removing comments

--- src/modules/population/router/population.router.js
+++ src/modules/population/router/population.router.js
@@ -6,23 +6,12 @@ import { asyncHandler } from "../../../middlewares/asyncHandler.js";
 import {  getAllPopulation, identification, identificationByDNA, updatePopulation ,adddPopulation, getAllPopulationDecrypted} from "../controller/population.controller.js";
 import getTextFileUploadMiddleware from "../../../middlewares/upload.js";
 const router=Router()
-// // router.post("/addpopulation",authenticate,autherize('technical'),validate(AddPopulationSchema,AddPopulationlQuery),asyncHandler(addPopulation))
+
 router.get('/getAllPopulation',authenticate,autherize('admin'),asyncHandler(getAllPopulation))
 router.get('/identification',authenticate,autherize('admin','technical'),asyncHandler(identification))
-// // router.post('/identificationByDNA',authenticate,autherize('admin','technical'),asyncHandler(identificationByDNA))
-// // router.put('/updatePopulation/:id',authenticate,autherize('admin'),validate(updatePopulationSchema,updatePopulationQuery),asyncHandler(updatePopulation))
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// router.post('/addpopulation',authenticate,autherize('technical'),getTextFileUploadMiddleware(),validate(AddPopulationnSchema,AddPopulationnlQuery), asyncHandler(addPopulation));
 router.put('/updatePopulation/:id',authenticate,autherize('admin'),getTextFileUploadMiddleware(),validate(updatePopulationSchema,updatePopulationQuery), asyncHandler(updatePopulation));
 router.post('/identificationByDNA',authenticate,autherize('admin','technical'),getTextFileUploadMiddleware(),asyncHandler(identificationByDNA))
-/////////////////////////////////test////////////////////////////////
 router.post('/addpopulation',authenticate,autherize('technical'),getTextFileUploadMiddleware(),validate(AddPopulationnSchema,AddPopulationnlQuery), asyncHandler(adddPopulation));
 router.get('/EisaAPI',asyncHandler(getAllPopulationDecrypted))
-
-
-
-
-
 
 export default router
